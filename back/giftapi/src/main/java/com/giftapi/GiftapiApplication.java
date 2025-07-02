6322f9f9d97a6d5a2554a396cc27823dc5de6d8b
C:\Users\EZEN\Desktop\HTMLCSS <lso1961042@naver.com>
2025-07-02 10:13:50 +0900

    no message

---
+++ back/giftapi/src/main/java/com/giftapi/GiftapiApplication.java
@@ -0,0 +1,13 @@
+package com.giftapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftapiApplication.class, args);
+	}
+
+}
---
+++ back/giftapi/src/test/java/com/giftapi/GiftapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giftapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
